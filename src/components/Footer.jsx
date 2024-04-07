1ec87ac76645210ebc41e32622303688224cae41
mmYM1037 <mmym7.3710@gmail.com>
2024-04-06 23:09:55 -0300

    feature: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
+import FooterInd from "./FooterInd";
+
 function Footer() {
     return (
         <div className="footer">
-            <h2>Footer</h2>
+            <div className="secciones">
+                <FooterInd titulo="Información" link1="Medios de pago" link2="Políticas"/>
+                <FooterInd titulo="check-us" link1="Acerca de HS" link2="Contacto"/>
+                <FooterInd titulo="Ayuda" link1="Cambios y devoluciones" link2="Compra segura"/>
+            </div>
+
+            <section class="copyright">
+                <p>Heaven Sports ©| Todos los derechos reservados.</p>
+            </section>
         </div>
     )
 }
